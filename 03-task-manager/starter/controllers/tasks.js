afd54f83b3d2e4456f9644785ff0b93752b8d642
Hanna <akhhanna20@gmail.com>
2024-12-18 10:27:00 -0500

    Final commit for Task Manager API - part 2

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -16,10 +16,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    const error = new Error(`Not Found`);
-    error.status = 404;
-    return next(error);
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
@@ -28,7 +25,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
@@ -40,12 +37,8 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
-  if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
-  }
-
   res.status(200).json({ task });
 });
 
